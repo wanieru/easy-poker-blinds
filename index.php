5fba3e906ae1724e89dbfd3490f8c1439929fc56
Unknown <github@wanieru.com>
2020-07-03 17:53:54 +0200

    Added html, css, js and bootstrap

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  $v = "1"
+?><!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>⏲️ Easy Blinds</title>
+  <meta name="description" content="Poker Clock">
+  <meta name="author" content="Wanieru">
+
+  <link rel="stylesheet" href="css/main.css?v=<?php echo $v; ?>">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/lib/bootstrap.min.css?v=<?php echo $v; ?>">
+
+</head>
+
+<body>
+  <div class="container pt-3 pb-5">
+
+  </div>
+  <script src="js/main.js?v=<?php echo $v; ?>"></script>
+</body>
+</html>
